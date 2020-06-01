e24419b2ea9b7edba6223cf4351f5a3d215f8c39
Daniel Röhrig <mail@danielroehrig.de>
2020-06-01 16:22:00 +0200

    Removed experiments

--- src/main.js
+++ src/main.js
@@ -5,18 +5,9 @@ import store from './store'
 import './../node_modules/bulma/css/bulma.css';
 
 Vue.config.productionTip = false
-const fs = require('fs')
-const path = require('path')
-
-// Expects myText.txt to be placed in public folder
-const fileLocation = path.join(__static, 'silence.mp3')
-const fileContents = fs.readFileSync(fileLocation, 'utf8')
-
-var appState = {};
 
 new Vue({
   router,
   store,
-  data: appState,
   render: function (h) { return h(App) }
-}).$mount('#app')
+}).$mount('#app');
