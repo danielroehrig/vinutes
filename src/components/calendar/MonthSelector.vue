b1f9339474f100b8c4c6c1b0b43198e7e896c178
Daniel Röhrig <mail@danielroehrig.de>
2020-06-06 18:24:40 +0200

    Make hitbox bigger

--- src/components/calendar/MonthSelector.vue
+++ src/components/calendar/MonthSelector.vue
@@ -2,9 +2,9 @@
     <section class="section">
         <div class="container">
             <nav class="columns is-vcentered">
-                <div class="column has-text-centered" v-on:click="moveToPreviousMonth"><span class="mdi mdi-arrow-left-thick mdi-48px" style="cursor: pointer"></span></div>
+                <div class="column has-text-centered" v-on:click="moveToPreviousMonth"><span class="icon is-large"><i class="mdi mdi-arrow-left-thick mdi-48px" style="cursor: pointer"></i></span></div>
                 <div class="column has-text-centered is-three-fifths"><h1 class="title is-1">{{ formattedDate }}</h1></div>
-                <div v-if="!isNextMonthFuture" class="column has-text-centered" v-on:click="moveToNextMonth"><span class="mdi mdi-arrow-right-thick mdi-48px" style="cursor: pointer"></span></div>
+                <div v-if="!isNextMonthFuture" class="column has-text-centered" v-on:click="moveToNextMonth"><span class="icon is-large"><i class="mdi mdi-arrow-right-thick mdi-48px" style="cursor: pointer"></i></span></div>
                 <div v-else class="column"></div>
             </nav>
         </div>
