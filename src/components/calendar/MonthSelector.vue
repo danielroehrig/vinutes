1c6bb6d0ee8b8de39b7339e4240659063cf457ac
Daniel Röhrig <mail@danielroehrig.de>
2020-06-06 16:45:51 +0200

    Month selector are arrows

--- src/components/calendar/MonthSelector.vue
+++ src/components/calendar/MonthSelector.vue
@@ -2,9 +2,9 @@
     <section class="section">
         <div class="container">
             <nav class="columns is-vcentered">
-                <div class="column has-text-centered" v-on:click="moveToPreviousMonth">Back</div>
+                <div class="column has-text-centered" v-on:click="moveToPreviousMonth"><span class="mdi mdi-arrow-left-thick mdi-48px" style="cursor: pointer"></span></div>
                 <div class="column has-text-centered is-three-fifths"><h1 class="title is-1">{{ formattedDate }}</h1></div>
-                <div class="column has-text-centered" v-on:click="moveToNextMonth">Forward</div>
+                <div class="column has-text-centered" v-on:click="moveToNextMonth"><span class="mdi mdi-arrow-right-thick mdi-48px" style="cursor: pointer"></span></div>
             </nav>
         </div>
     </section>
