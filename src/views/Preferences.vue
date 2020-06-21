1884d151dc1f3477ce6618da678ce2ab5df066af
Daniel Röhrig <mail@danielroehrig.de>
2020-06-21 10:08:07 +0200

    Start working on languages

--- src/views/Preferences.vue
+++ src/views/Preferences.vue
@@ -1,12 +1,27 @@
 <template>
-    <div>
-        <h1>Hello!</h1>
+    <div class="container">
+        <div class="field has-text-left">
+            <label class="label">Subject</label>
+            <div class="control">
+                <div class="select">
+                    <select>
+                        <option>Select dropdown</option>
+                        <option>With options</option>
+                    </select>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
     export default {
-        name: "Preferences"
+        name: "Preferences",
+        computed: {
+            languages() {
+
+            }
+        }
     }
 </script>
 
