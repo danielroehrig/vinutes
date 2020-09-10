276ddb948b8b0138cf8d499605054af59b33f3e6
Daniel Röhrig <mail@danielroehrig.de>
2020-09-10 21:52:32 +0200

    Start work on delete confirmation dialogue

--- src/views/Calendar.vue
+++ src/views/Calendar.vue
@@ -1,12 +1,28 @@
 <template>
-    <div>
-        <MonthSelector></MonthSelector>
-        <CalendarMonth></CalendarMonth>
+  <div>
+    <MonthSelector></MonthSelector>
+    <CalendarMonth></CalendarMonth>
+    <div class="modal is-active">
+      <div class="modal-background"></div>
+      <div class="modal-card">
+        <header class="modal-card-head">
+          <p class="modal-card-title">Delete Media</p>
+        </header>
+        <section class="modal-card-body">
+          Do you want to delete media for xxx-xx-xx from this timeline?
+        </section>
+        <footer class="modal-card-foot">
+          <button class="button">Cancel</button>
+          <button class="button is-danger">Delete</button>
+        </footer>
+      </div>
     </div>
+  </div>
 </template>
 <script>
 import CalendarMonth from '@/components/calendar/CalendarMonth'
 import MonthSelector from '@/components/calendar/MonthSelector'
+
 export default {
   components: { MonthSelector, CalendarMonth }
 }
