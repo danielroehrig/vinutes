4966d5955560598033782a748eee52ed331e92c5
Daniel Röhrig <mail@danielroehrig.de>
2020-09-17 21:48:48 +0200

    Test if date is correctly calculated

--- tests/components/dialog.delete-media.spec.js
+++ tests/components/dialog.delete-media.spec.js
@@ -4,6 +4,7 @@ import Vuex from 'vuex'
 import Vue from 'vue'
 import * as sc from '@/store-constants'
 import DeleteMediaDialog from '@/components/DeleteMediaDialog'
+import DailyMedia from '@/lib/DailyMedia'
 
 Vue.use(Vuex)
 
@@ -39,5 +40,35 @@ describe('DeleteMediaDialog.vue', () => {
     await wrapper.vm.$nextTick().then(() => {
       expect(modal.classes()).toContain('is-active')
     })
+  })
+  it('gets full date depending on language and date in mediafile', async () => {
+    const testMediaFile = new DailyMedia(2018, 3, 2, 'some/path', 'video')
+    const store = new Vuex.Store({
+      state: {
+        appState: sc.APP_STATE_CONFIRM_MEDIA_DELETE,
+        currentDaySelected: 2,
+        language: 'en',
+        calendarTimeStampFormat: 'll',
+        mediaFiles: {
+          2: testMediaFile
+        }
+      },
+      mutations: {
+        switchToGerman (state) {
+          state.language = 'de'
+        }
+      }
+    })
+    const wrapper = mountWithStore(store)
+    expect(wrapper.vm.mediaFile).toBe(testMediaFile)
+    expect(wrapper.vm.isVisible).toBe(true)
+    expect(wrapper.vm.fullDate).toBe('Mar 2, 2018')
+    store.commit('switchToGerman')
+    await wrapper.vm.$nextTick().then(() => {
+      expect(wrapper.vm.fullDate).toBe('2. März 2018')
+    })
+    // TODO: Switch mediaFile
+    // TODO: Set mediaFile to empty
+    // TODO: Test video still
   })
 })
