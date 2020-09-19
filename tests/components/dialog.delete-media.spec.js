1d6df1deb6a8b118cc5bbd5ed7baf27050968abc
Daniel Röhrig <mail@danielroehrig.de>
2020-09-19 21:12:28 +0200

    Test for switch media

--- tests/components/dialog.delete-media.spec.js
+++ tests/components/dialog.delete-media.spec.js
@@ -42,7 +42,7 @@ describe('DeleteMediaDialog.vue', () => {
     })
   })
   it('gets full date depending on language and date in mediafile', async () => {
-    const testMediaFile = new DailyMedia(2018, 3, 2, 'some/path', 'video')
+    const testMediaFile = new DailyMedia(2018, 3, 2, 'some/path', 'video', 0.0)
     const store = new Vuex.Store({
       state: {
         appState: sc.APP_STATE_CONFIRM_MEDIA_DELETE,
@@ -67,8 +67,36 @@ describe('DeleteMediaDialog.vue', () => {
     await wrapper.vm.$nextTick().then(() => {
       expect(wrapper.vm.fullDate).toBe('2. März 2018')
     })
-    // TODO: Switch mediaFile
-    // TODO: Set mediaFile to empty
-    // TODO: Test video still
+  })
+  it('switch media files', async () => {
+    const testMediaFile = new DailyMedia(2018, 3, 2, 'some/path', 'video')
+    const secondTestMediaFile = new DailyMedia(2018, 3, 4, 'some/other/path', 'video')
+    const store = new Vuex.Store({
+      state: {
+        appState: sc.APP_STATE_CONFIRM_MEDIA_DELETE,
+        currentDaySelected: 2,
+        language: 'en',
+        calendarTimeStampFormat: 'll',
+        mediaFiles: {
+          2: testMediaFile,
+          4: secondTestMediaFile
+        }
+      },
+      mutations: {
+        switchMediaFile (state) {
+          state.currentDaySelected = 4
+        }
+      }
+    })
+    const wrapper = mountWithStore(store)
+    expect(wrapper.vm.mediaFile).toBe(testMediaFile)
+    expect(wrapper.vm.isVisible).toBe(true)
+    expect(wrapper.vm.fullDate).toBe('Mar 2, 2018')
+    store.commit('switchMediaFile')
+    await wrapper.vm.$nextTick().then(() => {
+      expect(wrapper.vm.fullDate).toBe('Mar 4, 2018')
+      expect(wrapper.vm.mediaFile).toBe(secondTestMediaFile)
+      expect(wrapper.vm.isVisible).toBe(true)
+    })
   })
 })
