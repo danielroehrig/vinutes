fb09de6e81519a3469c03b66eb9a6455572ecaec
Daniel Röhrig <mail@danielroehrig.de>
2020-09-23 22:56:27 +0200

    Basic e2e test

--- tests/e2e/smoke.spec.js
+++ tests/e2e/smoke.spec.js
@@ -4,14 +4,18 @@ jest.setTimeout(50000)
 
 describe('Application launch', function () {
   it('opens a window', async function () {
-    const { stdout, url, stopServe, app } = await testWithSpectron(spectron)
-    // stdout is the log of electron:serve
-    console.log(`electron:serve returned: ${stdout}`)
-    // url is the url for the dev server created with electron:serve
-    console.log(`the dev server url is: ${url}`)
+    const { stopServe, app } = await testWithSpectron(spectron)
     // app is a spectron instance. It is attached to the dev server, launched, and waited for to load.
     expect(await app.client.getWindowCount()).toBe(1)
+    const thirdDay = await app.client.$('#calendarDay3')
+    await thirdDay.click()
+    const timelineInput = await app.client.$('#timelineCreationDialogInputTimelineName')
+    await timelineInput.setValue('New Timeline')
+    const timelineAcceptButton = await app.client.$('#timelineCreationDialogButtonSubmit')
+    await timelineAcceptButton.click()
+    const navbar = await app.client.$$('a.navbar-link')
+    expect(await navbar[0].getText()).toBe('New Timeline')
     // Before your tests end, make sure to stop the dev server and spectron
-    await stopServe()
+    // await stopServe()
   })
 })
