d153b73f53519f7721f7987dd7c90ff38b414173
Daniel Röhrig <mail@danielroehrig.de>
2020-10-06 22:00:07 +0200

    Create render time span dialog

---
+++ src/components/RenderTimeSpanDialog.vue
@@ -0,0 +1,95 @@
+<template>
+  <div class="modal" :class="{'is-active': isVisible}" id="deleteMediaDialog">
+    <div class="modal-background"></div>
+    <div class="modal-card">
+      <header class="modal-card-head">
+        <p class="modal-card-title">{{ $t('action.render-time-span') }}</p>
+      </header>
+      <section class="modal-card-body">
+        <div class="tabs is-centered">
+          <ul>
+            <li :class="{'is-active': tabSelected('whole')}"><a @click="selectTab('whole')">{{ $t('whole-time-line') }}</a></li>
+            <li :class="{'is-active': tabSelected('month')}"><a @click="selectTab('month')">{{ $t('month') }}</a></li>
+            <li :class="{'is-active': tabSelected('year')}"><a @click="selectTab('year')">{{ $t('year') }}</a></li>
+            <li :class="{'is-active': tabSelected('custom')}"><a @click="selectTab('custom')">{{ $t('custom') }}</a></li>
+          </ul>
+        </div>
+        <div :class="{'is-hidden': !tabSelected('whole')}">
+          <i>{{ $t('text.render-whole-timeline') }}</i>
+        </div>
+        <div :class="{'is-hidden': !tabSelected('month')}">
+          Month Stuff
+        </div>
+        <div :class="{'is-hidden': !tabSelected('year')}">
+          year Stuff
+        </div>
+        <div class="field" :class="{'is-hidden': !tabSelected('custom')}">
+          <div class="control">
+            <input type="date" id="renderTimeSpanDialogDateSpanChooser" ref='calendarTrigger'>
+          </div>
+        </div>
+      </section>
+      <footer class="modal-card-foot">
+        <button class="button">{{ $t('button.cancel') }}</button>
+        <button class="button is-success">{{ $t('button.render') }}</button>
+      </footer>
+    </div>
+  </div>
+</template>
+<script>
+import bulmaCalendar from 'bulma-calendar'
+
+export default {
+  name: 'RenderTimeSpanDialog',
+  data: function () {
+    return {
+      selectedTab: 'whole'
+    }
+  },
+  mounted () {
+    const calendar = bulmaCalendar.attach(this.$refs.calendarTrigger, {
+      type: 'date',
+      isRange: true,
+      dateFormat: 'DD.MM.YYYY',
+      displayMode: 'dialog',
+      showFooter: false
+    })[0]
+    calendar.on('select', e => console.log('gna'))
+  },
+  computed: {
+    isVisible () {
+      return true
+    }
+  },
+  methods: {
+    tabSelected (tab) {
+      return tab === this.selectedTab
+    },
+    selectTab (tab) {
+      this.selectedTab = tab
+    }
+  }
+}
+</script>
+<style>
+@import "~bulma-calendar/dist/css/bulma-calendar.min.css";
+
+/*.datepicker-nav, .datepicker-range{
+  background: red!important;
+}
+.datetimepicker-selection-day, .datepicker-date.datepicker-range.datepicker-range-start {
+  color: red!important;
+}
+div.datepicker-range-start{
+  background: red!important;
+}
+div.datepicker-range > button {
+  background: red!important;
+}
+
+div.datepicker-range-start > button, div.datepicker-range-end > button {
+  border-color: red!important;
+  color: red!important;
+  background: white!important;
+}*/
+</style>
