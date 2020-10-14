784ce7235432747da2585c63482c5292da06a7a5
Daniel Röhrig <mail@danielroehrig.de>
2020-10-14 22:49:19 +0200

    Adding metainformation to package.json
    To allow deb creation

--- vue.config.js
+++ vue.config.js
@@ -1,34 +1,37 @@
-const webpack = require("webpack");
+const webpack = require('webpack')
 
 module.exports = {
-    configureWebpack: {
-    },
-    pluginOptions: {
-      electronBuilder: {
-        externals: [
-          'better-sqlite3'
-        ],
-        preload: 'src/preload.js',
-        chainWebpackMainProcess: config => {
-                        config.plugin('define').tap(args => {
-                            args[0]['process.env.FLUENTFFMPEG_COV'] = false;
-                            return args;
-                        })
-                    },
-        builderOptions: {
-          extraResources: [
-            'public/**'
-          ],
-          asarUnpack: [
-            '**/node_modules/sharp/**/*'
-          ]
-        }
+  configureWebpack: {
+  },
+  pluginOptions: {
+    electronBuilder: {
+      externals: [
+        'better-sqlite3'
+      ],
+      preload: 'src/preload.js',
+      chainWebpackMainProcess: config => {
+        config.plugin('define').tap(args => {
+          args[0]['process.env.FLUENTFFMPEG_COV'] = false
+          return args
+        })
       },
-      i18n: {
-        locale: 'en',
-        fallbackLocale: 'en',
-        localeDir: 'locales',
-        enableInSFC: true
+      builderOptions: {
+        linux: {
+          target: ['AppImage', 'deb']
+        },
+        extraResources: [
+          'public/**'
+        ],
+        asarUnpack: [
+          '**/node_modules/sharp/**/*'
+        ]
       }
     },
+    i18n: {
+      locale: 'en',
+      fallbackLocale: 'en',
+      localeDir: 'locales',
+      enableInSFC: true
+    }
+  }
 }
