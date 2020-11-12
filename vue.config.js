6df976476e31475adc04884836603aabb29e70dd
Daniel Röhrig <mail@danielroehrig.de>
2020-11-12 22:46:41 +0100

    Fix missing silence.mp3 error

--- vue.config.js
+++ vue.config.js
@@ -44,6 +44,9 @@ module.exports = {
             'bin/macos/**'
           ]
         },
+        extraResources: [
+          'public/**'
+        ],
         asarUnpack: [
           '**/node_modules/sharp/**/*'
         ]
