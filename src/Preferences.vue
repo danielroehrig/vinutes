1cc385982502f92d160e5276a16db04d5ea70e45
Daniel Röhrig <mail@danielroehrig.de>
2021-05-04 21:41:44 +0200

    Forgot to update the date in the preferences dialog

--- src/Preferences.vue
+++ src/Preferences.vue
@@ -31,7 +31,7 @@
       </div>
       <footer class="footer">
         <div class="content">
-          <p class="has-text-weight-light is-size-7">Version 0.3.1 - 2021-02-21</p>
+          <p class="has-text-weight-light is-size-7">Version 0.3.2 - 2021-05-04</p>
           <p>
             <strong>Vinutes</strong> by <a href="https://github.com/danielroehrig" target="_blank">Daniel Röhrig</a>. The
             <a href="https://github.com/danielroehrig/vinutes" target="_blank">source code</a> is licensed
