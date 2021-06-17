da10fac33f5d8dcb995ec573d004577970daea84
Daniel Röhrig <mail@danielroehrig.de>
2021-06-17 22:33:33 +0200

    Log missing media type headers and add mp4

--- src/lib/VideoChecker.js
+++ src/lib/VideoChecker.js
@@ -1,8 +1,8 @@
 const { app } = require('electron')
 const path = require('path')
 const os = require('os')
-const fs = require('fs')
 const fsPromises = require('fs').promises
+const log = require('electron-log')
 
 let ffmpegPath = null
 const FfmpegCommand = require('fluent-ffmpeg')
@@ -21,49 +21,12 @@ switch (os.type()) {
 }
 FfmpegCommand.setFfmpegPath(ffmpegPath)
 
-const availableFormats = new Promise((resolve) => {
-  FfmpegCommand.getAvailableFormats(function (err, formats) {
-    if (err !== null) {
-      log.error('FFMPEG: could not get available formats')
-      app.exit(1)
-    }
-    resolve(formats)
-  })
-})
-const availableCodecs = new Promise((resolve) => {
-  FfmpegCommand.getAvailableCodecs(function (err, formats) {
-    if (err !== null) {
-      log.error('FFMPEG: could not get available codecs')
-      app.exit(1)
-    }
-    resolve(formats)
-  })
-})
-
-const probeVideo = (filePath) => {
-  return new Promise((resolve, reject) => {
-    console.log(filePath)
-    FfmpegCommand()
-      .input(filePath)
-      .ffprobe(function (err, metadata) {
-        console.log('Video Checker')
-        console.log('err: ' + err)
-        console.dir(metadata)
-        if (err !== null) {
-          reject(err)
-        } else {
-          resolve(metadata)
-        }
-      })
-  })
-}
-
 function getMediaHeader (filePath) {
   return new Promise((resolve, reject) => {
     fsPromises.open(filePath, 'r')
       .then(fd => {
-        const buffer = Buffer.alloc(16)
-        return fd.read(buffer, 0, 16, 0)
+        const buffer = Buffer.alloc(24)
+        return fd.read(buffer, 0, 24, 0)
       })
       .then((buffer) => {
         const mediaHeadHexCode = buffer.buffer.toString('hex')
@@ -83,6 +46,10 @@ function getMediaExtension (mediaHeadHexCode) {
   // The letter G gets repeated every 188 bytes and it CAN be a MPEG Transport Stream (MPEG-2 Part 1)
   if (mediaHeadHexCodeUpper.substr(0, 2) === '47') {
     return 'mpg'
+  }
+  // Quicktime Container (usually mp4)
+  if (mediaHeadHexCodeUpper.substr(8, 8) === '66747970') {
+    return 'mp4'
   }
   let type = null
   switch (mediaHeadHexCodeUpper.substr(0, 8)) {
@@ -116,12 +83,8 @@ function getMediaExtension (mediaHeadHexCode) {
     case '000001B3':
       type = 'mpg'
       break
-    // MP4
-    case '66747970':
-      if (mediaHeadHexCodeUpper.substr(8, 8) === '69736F6D') {
-        type = 'mp4'
-      }
-      break
+    default:
+      log.debug('Media head unknown magic bytes', mediaHeadHexCodeUpper)
   }
   return type
 }
