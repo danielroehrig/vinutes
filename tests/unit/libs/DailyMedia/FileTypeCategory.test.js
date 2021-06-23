86e172674ba404c9a73e479c4cd37be655fa153e
Daniel Röhrig <mail@danielroehrig.de>
2021-06-23 22:12:01 +0200

    Adjust unit test for new getMediaFromFile function

--- tests/unit/libs/DailyMedia/FileTypeCategory.test.js
+++ tests/unit/libs/DailyMedia/FileTypeCategory.test.js
@@ -1,9 +1,9 @@
-import { fileTypeCategory } from '@/lib/DailyMedia'
 import * as path from 'path'
+import { getMediaTypeFromFile } from '@/lib/MediumRecognizer'
 
 describe('Determine if file is video or image ', () => {
   it('correctly identifies images', () => {
-    expect(fileTypeCategory(path.join(__dirname, '..', '..', '_support', 'emptyJpg.jpg'))).toEqual('image')
-    expect(fileTypeCategory(path.join(__dirname, '..', '..', '_support', 'emptyPng.png'))).toEqual('image')
+    expect(getMediaTypeFromFile(path.join(__dirname, '..', '..', '_support', 'emptyJpg.jpg'))).resolves.toBe('image')
+    expect(getMediaTypeFromFile(path.join(__dirname, '..', '..', '_support', 'emptyPng.png'))).resolves.toBe('image')
   })
 })
