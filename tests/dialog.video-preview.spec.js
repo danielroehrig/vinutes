5e35e61f086e6e4b8bee9d5b3a102497a48c9e98
Daniel Röhrig <mail@danielroehrig.de>
2021-11-21 22:20:02 +0100

    Added some silly tests, for the UI

--- tests/dialog.video-preview.spec.js
+++ tests/dialog.video-preview.spec.js
@@ -8,6 +8,24 @@ Vue.use(Vuex)
 const $t = jest.fn()
 Vue.use(Buefy)
 
+let playStub, pauseStub
+
+afterEach(() => {
+  console.log('after each called')
+  jest.restoreAllMocks()
+})
+
+beforeEach(() => {
+  playStub = jest
+    .spyOn(window.HTMLMediaElement.prototype, 'play')
+    .mockImplementation(() => {
+    })
+  pauseStub = jest
+    .spyOn(window.HTMLMediaElement.prototype, 'pause')
+    .mockImplementation(() => {
+    })
+}
+)
 describe('VideoPlayer.vue', () => {
   const wrapper = shallowMount(VideoPlayer, {
     store: new Vuex.Store({
@@ -23,9 +41,42 @@ describe('VideoPlayer.vue', () => {
   })
   const playPauseButton = wrapper.find('#videoPlayerPlayPauseButton')
   const loopToggleButton = wrapper.find('#buttonTogglePlayLooped')
-  it('show the play button on startup', () => {
+
+  it('shows the play button on startup', async () => {
+    jest
+      .spyOn(window.HTMLMediaElement.prototype, 'paused', 'get')
+      .mockImplementation(() => {
+        return true
+      })
     expect(playPauseButton.exists()).toBe(true)
     expect(playPauseButton.find('i').classes('mdi-play')).toBe(true)
+  })
+  it('clicks play button which becomes the pause button', async () => {
+    jest
+      .spyOn(window.HTMLMediaElement.prototype, 'paused', 'get')
+      .mockImplementation(() => {
+        return true
+      })
+    expect(playPauseButton.exists()).toBe(true)
+    expect(playPauseButton.find('i').classes('mdi-play')).toBe(true)
+    playPauseButton.trigger('click')
+    expect(playStub).toHaveBeenCalled()
+    await wrapper.vm.$nextTick().then(() => {
+      expect(playPauseButton.find('i').classes('mdi-play')).toBe(false)
+      expect(playPauseButton.find('i').classes('mdi-pause')).toBe(true)
+    })
+  })
+  it('clicks the pause button and reverts to play', async () => {
+    jest
+      .spyOn(window.HTMLMediaElement.prototype, 'paused', 'get')
+      .mockImplementation(() => {
+        return false
+      })
+    playPauseButton.trigger('click')
+    expect(pauseStub).toHaveBeenCalled()
+    await wrapper.vm.$nextTick().then(() => {
+      expect(playPauseButton.find('i').classes('mdi-play')).toBe(true)
+    })
   })
   it('shows the loop button toggled of', () => {
     expect(loopToggleButton.exists()).toBe(true)
